f861061e5511146bea89c166036b78791c804359
lumaeye <lumaeye@gmail.com>
2020-07-05 16:24:29 +0800

    20200705-1

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log("我是Js!");
+
+var a =10;
+var b =1.5;
+var c ="文字"
+var d =true;
+
+console.log(a);
+
+a = a + 1;
+
+function function A() {
+    console.log("我是函式A");
+}
+//需要呼叫才會執行
+functionA();
+functionA();
+
